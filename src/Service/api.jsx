67fb25f5b3574315ed288cc1e7795e7922efdf51
Neeraj-4 <76507348+Neeraj-4@users.noreply.github.com>
2023-04-05 10:56:12 +0530

    Update api.jsx

--- src/Service/api.jsx
+++ src/Service/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'https://servermuczi.onrender.com';
+const URL = process.env.REACT_APP_SERVER_URL;
 export const Authantication = async (data) => {
     try {
         return await axios.post(`${URL}/wish`, data)
